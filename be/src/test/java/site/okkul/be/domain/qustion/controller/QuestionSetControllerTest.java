ce6c39b2251cabd496195c9c7f878b280132e5da
최성연 <chltjddus357@naver.com>
2026-02-02 12:52:46 +0900

    [BE] fix : flyway sql 파일 및 테스트 프로필 수정

--- be/src/test/java/site/okkul/be/domain/qustion/controller/QuestionSetControllerTest.java
+++ be/src/test/java/site/okkul/be/domain/qustion/controller/QuestionSetControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import site.okkul.be.domain.qustion.dto.QuestionSetRequest;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("local")
 class QuestionSetControllerTest {
 
 	@Autowired
